c4b6829fe2d67a6344dac2bfee434648cd250122
Talon Meyer <talonmeyer12@gmail.com>
2022-12-22 21:00:01 -0800

    added to js file

--- script.js
+++ script.js
@@ -2,36 +2,44 @@ var numTies = 0;
 var numWins = 0;
 var numLosses = 0;
 
-var userInput = window.prompt("Enter R, P, or S (for rock paper or scissors). :)");
-
 var opponentInputList = ['R', 'P', 'S'];
 
-var opponentInput = opponentInputList[getNum()];
-
-check(userInput, opponentInput);
+var countinue = true;
+while (countinue) {
+    var userInput = window.prompt("Enter R, P, or S (for rock paper or scissors). :)");
+    var opponentInput = opponentInputList[getNum()];
+    window.alert("The computer choese " + opponentInput);
+    check(userInput, opponentInput);
+    var confirm = window.confirm("Would you like to countinue?");
+    if (!confirm) {
+        countinue = false;
+    }
+}
 
 window.alert("Tally:\nWins: " + numWins + "\nLosses: " + numLosses + "\nTies: " + numTies);
-
-
 
 function getNum() {
     return Math.floor(Math.random() * 3);
 }
 
 function check(user, comp) {
+    var outcome = null;
     if ((user === 'R' && comp === 'R') || 
         (user === 'P' && comp === 'P') ||
         (user === 'S' && comp === 'S')) {
-            window.alert("You tied with the computer :]");
+            window.confirm("You tied with the computer :]");
             numTies++;
+            outcome = 'tie';
     } else if ((user === 'R' && comp === 'S') ||
                 (user === 'P' && comp === 'R') ||
                 (user === 'S' && comp === 'P')) {
-                    window.alert("Congrats!!! You won!!!");
+                    window.confirm("Congrats!!! You won!!!");
                     numWins++;
+                    outcome = 'win';
     } else {
-        window.alert("Damn bro you lost :( Hold this L for me!");
+        window.confirm("Damn bro you lost :( Hold this L for me!");
         numLosses++;
+        outcome = 'loss';
     }
-
+    return outcome;
 };
